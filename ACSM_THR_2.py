b70904ec566d2609b02c93c986a5ed35532ff96e
owestryquestionnaire-ai <owestryquestionnaire@gmail.com>
2026-04-23 13:07:01 +0800

    Update ACSM_THR_2.py

--- ACSM_THR_2.py
+++ ACSM_THR_2.py
@@ -149,7 +149,7 @@ with tab2:
     st.header("已知醫療狀況") 
     st.write("您是否有以下任何已知醫療狀況？") 
     
-    disease_cardiovascular = st.checkbox("已知心血管疾病 (例如：冠心病、心臟病發作、中風、心臟衰竭)", key="d_cardio") 
+    disease_cardiovascular = st.checkbox("已知心血管疾病 (例如：冠心病、心臟病、中風、心臟衰竭、心律不正)", key="d_cardio") 
     disease_metabolic = st.checkbox("已知代謝疾病 (例如：糖尿病、甲狀腺疾病)", key="d_metabolic") 
     disease_renal = st.checkbox("已知腎臟疾病", key="d_renal") 
 
